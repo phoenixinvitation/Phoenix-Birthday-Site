88662dd483f15f0c01ed4997a414aedc62b5c502
Santhosh0077 <santhoshsankar1887@gmail.com>
2026-04-26 21:09:07 +0530

    update: site change

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,11 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  cloudflare: false,
   tanstackStart: {
+    server: {
+      preset: "vercel",
+    },
     prerender: {
       enabled: true,
       autoSubfolderIndex: true,
